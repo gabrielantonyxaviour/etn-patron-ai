045ff4af7f951881114c44de378238aa12909029
gabrielantonyxaviour <gabrielantonyxaviour.24ec@licet.ac.in>
2025-03-03 19:43:53 -0700

    add electroneum

--- frontend/components/providers/web3-provider.tsx
+++ frontend/components/providers/web3-provider.tsx
@@ -1,10 +1,27 @@
 "use client";
 
 import { ReactNode } from "react";
-import { DynamicContextProvider } from "@dynamic-labs/sdk-react-core";
+import {
+  DynamicContextProvider,
+  DynamicWidget,
+} from "@dynamic-labs/sdk-react-core";
+import { DynamicWagmiConnector } from "@dynamic-labs/wagmi-connector";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { http } from "viem";
+import { createConfig, WagmiProvider } from "wagmi";
+import { electroneum, electroneumTestnet } from "viem/chains";
 import { EthereumWalletConnectors } from "@dynamic-labs/ethereum";
-import { ViemWagmiWeb3Adapter } from "@dynamic-labs/wagmi-connector";
 
+const config = createConfig({
+  chains: [electroneum, electroneumTestnet],
+  multiInjectedProviderDiscovery: false,
+  transports: {
+    [electroneum.id]: http(),
+    [electroneumTestnet.id]: http(),
+  },
+});
+
+const queryClient = new QueryClient();
 interface Web3ProviderProps {
   children: ReactNode;
 }
@@ -13,40 +30,17 @@ export function Web3Provider({ children }: Web3ProviderProps) {
   return (
     <DynamicContextProvider
       settings={{
-        environmentId: process.env.NEXT_PUBLIC_DYNAMIC_PROJECT_ID || "",
+        environmentId: process.env.NEXT_PUBLIC_DYNAMIC_ENVIRONMENT_ID || "",
         walletConnectors: [EthereumWalletConnectors],
-        web3Adapters: [ViemWagmiWeb3Adapter],
-        evmNetworks: [
-          {
-            id: 5201420, // Electroneum mainnet
-            name: "Electroneum",
-            chainId: "0x4F63BC", // hex format
-            displayName: "Electroneum",
-            rpcUrls: ["https://rpc.electroneum.com"],
-            blockExplorerUrls: ["https://explorer.electroneum.com"],
-            nativeCurrency: {
-              name: "Electroneum",
-              symbol: "ETN",
-              decimals: 18,
-            },
-          },
-          {
-            id: 5201421, // Electroneum testnet
-            name: "Electroneum Testnet",
-            chainId: "0x4F63BD", // hex format
-            displayName: "Electroneum Testnet",
-            rpcUrls: ["https://testnet-rpc.electroneum.com"],
-            blockExplorerUrls: ["https://testnet-explorer.electroneum.com"],
-            nativeCurrency: {
-              name: "Electroneum",
-              symbol: "ETN",
-              decimals: 18,
-            },
-          },
-        ],
       }}
     >
-      {children}
+      <WagmiProvider config={config}>
+        <QueryClientProvider client={queryClient}>
+          <DynamicWagmiConnector>
+            <DynamicWidget />
+          </DynamicWagmiConnector>
+        </QueryClientProvider>
+      </WagmiProvider>
     </DynamicContextProvider>
   );
 }
